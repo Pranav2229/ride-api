97ea34dcd4388d6e7111ecb9b78838f9c8f1ac9b
unknown <pranavpatil956@gmail.com>
2026-07-18 23:06:02 +0530

    Connection changed db

--- src/middleware/db.connection.js
+++ src/middleware/db.connection.js
@@ -7,17 +7,47 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
+
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 // Check DB Connection
 pool.connect((err, client, release) => {
+
   if (err) {
-    return console.error("❌ Database connection failed:", err.message);
+    return console.error("❌ Database connection failed:", err);
   }
 
   console.log("✅ PostgreSQL Database connected successfully");
 
-  release(); // release client back to pool
+  release();
+
 });
 
 module.exports = pool;
+
+// const { Pool } = require("pg");
+// require("dotenv").config();
+
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   database: process.env.DB_NAME,
+// });
+
+// // Check DB Connection
+// pool.connect((err, client, release) => {
+//   if (err) {
+//     return console.error("❌ Database connection failed:", err.message);
+//   }
+
+//   console.log("✅ PostgreSQL Database connected successfully");
+
+//   release(); // release client back to pool
+// });
+
+// module.exports = pool;
